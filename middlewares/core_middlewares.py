145af0d054cd9b7cd55bf90da94d8d1373ca30b8
Ssentiago <arseny317@gmail.com>
2024-02-16 20:04:24 +0300

    немного рефакторинга...

--- middlewares/core_middlewares.py
+++ middlewares/core_middlewares.py
@@ -17,26 +17,18 @@ class SomeMiddleWare(BaseMiddleware):
         # подготавливаем всё нужное нам - данные юзера, его состояние и id
         user_state: FSMContext = data['state']
         user_data = await user_state.get_data()
-        user_id = event.from_user.id
         state = data['raw_state']
 
         if 'callback_data' in data:
-
-            # если пользователь находится сейчас в состоянии игры на одном компьютере, то дальнейшая обработка не требуется
+            # если пользователь играет сам с собой, то обрабатываем как обычно
             if state == Game.two_players_on_one_computer.state:
                 return await handler(event, data)
 
-            # проверяем, что пользователь находится в состоянии противоборства с другим пользователем
+            # проверяем, что пользователь играет против другого игрока
             if state == Game.player_vs_player.state:
-                # достаем id и состояния пользователей из пула
-                pair: tuple[int, FSMContext, int, FSMContext] = await get_the_pair(Service.game_pool, user_id)
-
-                # достаем знак пользователя и сразу же подготавливаем следующий знак
                 user_sign: str = user_data['sign']
-                next_sign = user_sign == '✕' and 'O' or '✕'
 
-
-                # если пользователь имеет право сейчас ходить, то обрабатываем его ход, если нет - шлём об этом уведомление
+                # если пользователь имеет право сейчас ходить, то отправляем его на обработку, если нет - шлём об этом уведомление
                 if user_sign == user_data['playing_now']:
                     return await handler(event, data)
                 else:
